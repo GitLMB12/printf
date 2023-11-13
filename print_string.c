9558f03bf0845ae0a2994ef49f02bacc2a503dc0
GitLMB12 <mhamedbenzaquiii@gmail.com>
2023-11-13 06:41:26 -0800

    printf

--- print_string.c
+++ print_string.c
@@ -1,15 +1,24 @@
 #include "main"
 
+/**
+ * print_string - prints a string
+ * @str: numberof arguements
+ * @i: the printed characters
+ * Return: printed charcaters
+ */
 
-int print_string(const char *str) {
-    int i = 0;
-    if (str == NULL) {
-        return write(1, "(Null)", 6);
-    }
-    while (str[i]) {
-        _putchar(str[i]);
-        i++;
-    }
-    return i;
+int print_string(const char *str, int i)
+{
+	 i = 0;
+
+	if (str == NULL)
+	{
+		return (write(1, "(Null)", 6));
+	}
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	return (i);
 }
-
